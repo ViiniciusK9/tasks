3db1ed8e0b69ddfc4f56d3f7403e7feccdbd9ab9
ViniciusK9 <viniciuskoncicoski@gmail.com>
2021-10-31 09:27:35 -0300

    up

--- a.cpp
+++ a.cpp
@@ -4,10 +4,32 @@
 #include <algorithm>
 #include <stack>
 #include <string>
-#include <set>
+#include <map>
+#include <vector>
 
 using namespace std;
 
 #define DBG(x) cout << "[" << #x << "]: " << x << endl
 #define F(x) std::fixed <<std::setprecision(1)<<(x)
 
+int main(){
+    string a;
+    int n;
+    map<string, int> M;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> a;
+        if(M.find(a) != M.end()){
+            M[a]++;
+        }else{
+            M[a] = 1;
+        }
+    }
+    
+
+    for (map<string,int>::iterator it=M.begin(); it!=M.end(); ++it){
+      cout<< it->first<< " aparece "<< it->second<< " vez(es)" << '\n';
+    }
+    
+    return 0;
+}
