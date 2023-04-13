c3b597e5e5194a31ecabe1f132857b9aa964bccc
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2023-04-13 11:31:24 -0300

    Tentando corrigir o problema 1023 da plataforma beecrowd. (Ainda não consegui).

--- beecrowd/bee/bee_1023.cpp
+++ beecrowd/bee/bee_1023.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 #define DBG(x) cout << "[" << #x << "]: " << x << endl
-#define F(x) std::fixed <<std::setprecision(1)<<(x)
+#define F(x) std::fixed <<std::setprecision(2)<<(x)
 
 int main(int argc, char const *argv[])
 {
@@ -12,33 +12,46 @@ int main(int argc, char const *argv[])
 
 
     int n, p, qnt, _case = 1;
-
+    double mid, peo;
+    cin >> n;
     while (n)
     {
-        map<int, int> vet;
+        if (_case != 1)
+        {
+            cout << '\n';
+        }
+        
+        vector<pair<int, int>> vet;
+        peo = mid = 0;
+
         for (int i = 0; i < n; i++)
         {
             cin >> p >> qnt;
+            mid += qnt;
+            peo += p;
             qnt /= p;
-            vet[qnt] = p;
+
+            vet.push_back(pair<int, int> (qnt, p));
         }
         cout << "Cidade# " << _case << ":\n";
         _case++;
         int aux = 0;
+        sort(vet.begin(), vet.end());
         for (auto &j : vet)
         {
             if (aux == 0)
             {
-                cout << j.second << "-" << j.first << " ";
+                cout << j.second << "-" << j.first;
                 aux = 1;
             } else {
-
+                cout << " " << j.second << "-" << j.first;
             }
             
             
         }
-        
-        
+        cout << '\n';
+        cout << "Consumo medio: " << floorf(mid/peo * 100) / 100 << " m3.\n";
+        cin >> n;
     }
     
 
