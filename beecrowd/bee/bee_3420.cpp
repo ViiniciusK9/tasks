cf03fd5322dafac0f4bae47b0bb89c588c75bbbd
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2023-04-23 19:19:10 -0300

    Tentando realizar o exercicio 3420

--- beecrowd/bee/bee_3420.cpp
+++ beecrowd/bee/bee_3420.cpp
@@ -5,30 +5,47 @@ using namespace std;
 #define DBG(x) cout << "[" << #x << "]: " << x << endl
 #define F(x) std::fixed <<std::setprecision(1)<<(x)
 
+#define MAX (int)10e18 / 3
+
 int main(int argc, char const *argv[])
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    long long v = 1, pro, at;
+    long long ult = 0, aux;
+    vector<long long> vet;
+    long long n = 1;
+    while (ult <= MAX && ult >= 0)
+    {
+        ult = n * (n+1) / 2;
+        //ult += (n-1) + (n * 2);
+        n++;
+        vet.push_back(ult);
+    }
+    //cout << ult << '\n';
 
-    at = 2;
-    long long i = 1;
-
-    while (at < 10e18 && at > 0 )
-    {   
-        pro = (i + 1) * 2;
-        at += pro + i;
-        //cout << at << '\n';
-        i++;
-        cout << i  << " : " << at << '\n';
-        if (i == 101)
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        cin >> aux;
+        vector<long long>::iterator card = upper_bound(vet.begin(), vet.end(), aux/3);
+        //cout << aux << '\n';
+        DBG(vet[card - vet.begin()]);
+        long long cards = card - vet.begin();
+        //DBG(cards);
+        //DBG(vet[card - vet.begin()-1] * 3 - (card - vet.begin()));
+        if (vet[card - vet.begin()-1] * 3 - (card - vet.begin())  <= aux)
         {
-            break;
+            cout << cards +1 << '\n';
+        } else {
+            cout << cards << '\n';
         }
+        
         
     }
 
+    
     
     return 0;
 }
