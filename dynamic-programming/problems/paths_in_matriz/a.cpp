ac1e8087a4a443205aaae2dbc5344b3254cc6631
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2024-03-27 09:39:17 -0300

    Adding version of the paths in matrix problem with bottom-up and bottom-up approach with memory optimization

--- dynamic-programming/problems/paths_in_matriz/a.cpp
+++ dynamic-programming/problems/paths_in_matriz/a.cpp
@@ -7,6 +7,11 @@ using namespace std;
 
 array<array<int, 10>, 10> matriz;
 array<array<int, 10>, 10> memoization;
+array<array<int, 10>, 10> tabulation;
+
+array<int, 10> previous;
+array<int, 10> dp;
+
 int n, m;
 
 
@@ -44,17 +49,75 @@ int top_down_memoization(int i, int j) {
     if (memoization[i][j] != 0) return memoization[i][j];
 
     if (i == n-1) {
-        memoization[i][j] += top_down(i, j+1);
+        memoization[i][j] = top_down_memoization(i, j+1);
         return memoization[i][j];
     }
 
     if (j == m-1) {
-        memoization[i][j] += top_down(i + 1, j);
+        memoization[i][j] = top_down_memoization(i + 1, j);
         return memoization[i][j];
     }
 
-    memoization[i][j] += top_down(i + 1, j) + top_down(i, j+1);
+    memoization[i][j] = top_down_memoization(i + 1, j) + top_down_memoization(i, j+1);
     return memoization[i][j];
+}
+
+int bottom_up(int ii, int jj) {
+
+    tabulation[0][0] = 1;
+
+    for (int i = 1; i < m; i++)
+    {
+        tabulation[0][i] = (matriz[0][i] == 0) ? tabulation[0][i-1] : 0;
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        tabulation[i][0] = (matriz[i][0] == 0) ? tabulation[i-1][0] : 0;
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j < m; j++)
+        {
+            tabulation[i][j] = (matriz[i][j] == 0) ? (tabulation[i][j-1] + tabulation[i-1][j]) : 0;
+        }
+    }
+
+    return tabulation[ii][jj];
+}
+
+int bottom_up_memory_optimization(int ii, int jj){
+
+    previous[0] = 1;
+
+    for (int i = 1; i < m; i++)
+    {
+        previous[i] = (matriz[0][i] == 0) ? previous[i-1] : 0;
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        cout << previous[i] << " ";
+    }
+    cout << '\n';
+
+    for (int i = 1; i < n; i++)
+    {
+        dp[0] = (matriz[i][0] == 0) ? previous[0] : 0;
+        for (int j = 1; j < m; j++)
+        {
+            dp[j] = (matriz[i][j] == 0) ? (dp[j-1] + previous[j]) : 0;
+        }
+        previous = dp;
+        for (int i = 0; i < m; i++)
+        {
+            cout << previous[i] << " ";
+        }
+        cout << '\n';
+    }
+
+    return previous[jj];
 }
 
 int main(int argc, char const *argv[])
@@ -89,6 +152,7 @@ int main(int argc, char const *argv[])
     for (int i = 0; i < 10; i++)
     {
         fill(memoization[i].begin(), memoization[i].end(), 0);
+        fill(tabulation[i].begin(), tabulation[i].end(), 0);
     }
 
     cout << top_down_memoization(0, 0) << '\n';
@@ -101,6 +165,23 @@ int main(int argc, char const *argv[])
         }
         cout << '\n';
     }
+    cout << '\n';
+    cout << bottom_up(n-1, m-1) << '\n';
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << tabulation[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+
+    cout << "\n\n\n";
+
+    int ans = bottom_up_memory_optimization(n-1, m-1);
+    
+    cout << ans << '\n';
 
     return 0;
 }
