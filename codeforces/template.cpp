7792743a38d213b95903149fbcdcd88969a7ae31
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2024-04-12 08:13:39 -0300

    tentativa problema 466-c

--- codeforces/template.cpp
+++ codeforces/template.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 #define DBG(x) cout << "[" << #x << "]: " << x << endl
 #define F(x) std::fixed <<std::setprecision(1)<<(x)
+#define F first 
+#define S second 
+#define PB push_back 
+#define MP make_pair
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<int, pi> pii;
+
 
 int main(int argc, char const *argv[])
 {
