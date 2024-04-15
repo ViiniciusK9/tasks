0ed8b4f83327bea0a8618b6ca73ed69771447ce7
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2024-04-15 07:52:47 -0300

    adding edit distance problem

---
+++ codeforces/894/a.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define DBG(x) cout << "[" << #x << "]: " << x << endl
+#define FF(x) std::fixed <<std::setprecision(1)<<(x)
+#define F first 
+#define S second 
+#define PB push_back 
+#define MP make_pair
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef pair<int, pi> pii;
+
+int main(int argc, char const *argv[])
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    
+
+    
+    return 0;
+}
+
+// g++ -O2 -Wall nome.cpp -o nome
--- dynamic-programming/problems/edit_distance/a.cpp
+++ dynamic-programming/problems/edit_distance/a.cpp
@@ -5,6 +5,33 @@ using namespace std;
 #define DBG(x) cout << "[" << #x << "]: " << x << endl
 #define F(x) std::fixed <<std::setprecision(1)<<(x)
 
+const int MAX = 100;
+
+string s1, s2;
+int n1, n2;
+int memo[MAX][MAX];
+
+int solve(int i, int j)
+{
+    if (i == n1) {
+        return abs(j - n2);
+    }
+
+    if (j == n2) {
+        return abs(i - n1);
+    }
+
+    if (memo[i][j] != 0) return memo[i][j];
+
+    if (s1[i] == s2[j]) {
+        memo[i][j] = solve(i+1, j+1);
+        return memo[i][j];
+    }
+
+    memo[i][j] = 1 + min({solve(i+1, j), solve(i+1, j+1), solve(i, j+1)});
+    return memo[i][j];
+}
+
 int main(int argc, char const *argv[])
 {
     ios_base::sync_with_stdio(0);
@@ -12,7 +39,13 @@ int main(int argc, char const *argv[])
 
     
 
+    cin >> s1 >> s2;
+    n1 = s1.size();
+    n2 = s2.size();
+
     
+    cout << solve(0, 0) << '\n';
+
     return 0;
 }
 
