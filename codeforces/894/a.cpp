139d6d447b832a2398d8e125b7158036a02417a3
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2024-04-16 07:14:36 -0300

    adding problem

--- codeforces/894/a.cpp
+++ codeforces/894/a.cpp
@@ -19,7 +19,40 @@ int main(int argc, char const *argv[])
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
+    long long ans = 0;
+    int qnt = 0;
+    vi v1;
+    vi v2;
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == 'Q'){
+            qnt++;
+        }
+        v1.PB(qnt);
+    }
+
+    qnt = 0;
+    for (int i = s.length()-1; i >= 0; i--)
+    {
+        if (s[i] == 'Q'){
+            qnt++;
+        }
+        v2.PB(qnt);
+    }
+
+    reverse(v2.begin(), v2.end());
+
+    for (int i = 1; i < s.length()-1; i++)
+    {
+        if (s[i] == 'A'){
+            ans += v1[i-1] * v2[i+1];
+        }
+    }
     
+    cout << ans << '\n';
 
     
     return 0;
