86379cb5688204d59a56c1a39aa5f7a43ccfa7d3
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2024-07-13 16:45:19 -0300

    adding problems

---
+++ codeforces/1846/a.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define DBG(x) cout << "[" << #x << "]: " << x << endl
+#define F(x) std::fixed <<std::setprecision(1)<<(x)
+#define f first 
+#define s second 
+#define pb push_back 
+#define mp make_pair
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+
+int main(int argc, char const *argv[])
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int x, w;
+
+            cin >> x >> w;
+
+            if (x > w) {
+                ans++;
+            }
+        }
+
+        cout << ans << '\n';
+        
+    }
+    
+
+    
+    return 0;
+}
+
+// g++ -O2 -Wall nome.cpp -o nome
