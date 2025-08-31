8ef0563daaac9b4ac7676dd8f5f43fa1453c8737
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2025-08-31 15:54:05 -0300

    feat: add problem a

--- codeforces/2094/a.cpp
+++ codeforces/2094/a.cpp
@@ -19,6 +19,21 @@ int main(int argc, char const *argv[])
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
+    int t;
+
+    cin >> t;
+
+    string a, b, c;
+
+    for (int i = 0; i < t; i++)
+    {
+        cin >> a >> b >> c;
+        cout << a[0] << b[0] << c[0] << '\n';
+    }
+
+    
+
+    
 
 
     
